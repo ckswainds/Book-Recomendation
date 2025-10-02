d6058bc08e3bf7204dcb505f8f99966196b96dc0
Chandan <chandankumarswain2468@gmail.com>
2025-10-02 12:22:39 +0530

    Data ingested from API

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -1,1 +1,9 @@
-GOOGLE_BOOKS_API="Google-api"
+GOOGLE_BOOKS_API="Google_api"
+
+
+
+#Data ingestion Constants
+FEATURE_STORE_DIRNAME:str="data"
+FEATURE_STORE_EXTERNAL_DATA_FOLDER:str="external"
+BOOKS_DATA_FILENAME:str="Ml_books.json"
+PAPERS_DATA_FILENAME:str="all_papers.json"
