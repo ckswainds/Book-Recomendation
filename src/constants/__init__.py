71eb5a7721623954d5774d5ae1409cf62b1accab
Chandan <chandankumarswain2468@gmail.com>
2025-10-03 21:49:23 +0530

    Feature building done

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -14,3 +14,11 @@ CLEANED_DATA_DIRNAME:str="data"
 CLEANED_DATA_FOLDER:str="raw"
 CLEANED_BOOKS_DATA_FILENAME:str="Ml_books.csv"
 CLEANED_PAPERS_DATA_FILENAME:str="all_papers.csv"
+
+
+
+#FeatureBuilding constants
+MODIFIED_DATA_DIRNAME:str="data"
+MODIFIED_DATA_FOLDER:str="interim"
+MODIFIED_BOOKS_DATA_FILENAME:str="modified_books.csv"
+MODIFIED_PAPERS_DATA_FILENAME:str="modified_papers.csv"
