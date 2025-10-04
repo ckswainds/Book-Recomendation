8e3a888e399b5c3c969038b173f3bc0521c2152c
Chandan <chandankumarswain2468@gmail.com>
2025-10-04 12:37:43 +0530

    Add model trainer stage and implement recommendation model with TF-IDF processing

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -22,3 +22,15 @@ MODIFIED_DATA_DIRNAME:str="data"
 MODIFIED_DATA_FOLDER:str="interim"
 MODIFIED_BOOKS_DATA_FILENAME:str="modified_books.csv"
 MODIFIED_PAPERS_DATA_FILENAME:str="modified_papers.csv"
+
+
+
+#Model trainer constants
+MODEL_OUTPUT_DIR:str="data"
+MODEL_OUTPTUT_DATA_FOLDER:str="processed"
+BOOK_TF_IDF_MODEL:str="book_tfidf_vectorizer.pkl"
+PAPER_TF_IDF_MODEL:str="paper_tfidf_vectorizer.pkl"
+BOOK_TFIDF_MATRIX:str="book_tfidf_matrix.npz"
+PAPER_TFIDF_MATRIX:str="paper_tfidf_matrix.npz"
+BOOKS_FINAL_FILE:str="books_final.csv"
+PAPERS_FINAL_FILE:str="papers_final.csv"
