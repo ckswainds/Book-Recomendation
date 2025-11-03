879db9c9a9eb83afe9fe45fc8100150e94bfe0be
Chandan <chandankumarswain2468@gmail.com>
2025-11-03 19:25:39 +0530

    Changed Setup and pyproject

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="app_src",
+    version="0.0.1",
+    description="A Project on Book and Research paper Recommendation.",
+    author="chandan",
+    author_email="chandankumarswain3467@gmail.com",
+    packages=find_packages(),
+)
