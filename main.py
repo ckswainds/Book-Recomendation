0fc565bafe874c135f253377750cdcae43c3e35d
Chandan <chandankumarswain2468@gmail.com>
2025-11-03 19:16:15 +0530

    Removed the setup and pyproject.toml

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 import os,sys
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-SRC_PATH = os.path.join(ROOT_DIR, "src")
-if SRC_PATH not in sys.path:
-    sys.path.insert(0, SRC_PATH)
 import streamlit as st
 import json
 import ast
